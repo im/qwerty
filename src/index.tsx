73b09c881d7d355b219aefa211dbcd6c82e4d489
chengzi <wojiaochengyicheng@gmail.com>
2025-05-24 07:09:31 +0800

    update

--- src/index.tsx
+++ src/index.tsx
@@ -33,20 +33,20 @@ function Root() {
       : document.documentElement.classList.remove("dark");
   }, [darkMode]);
 
-  const [isMobile, setIsMobile] = useState(window.innerWidth <= 600);
+  const [isMobile, setIsMobile] = useState();
 
-  useEffect(() => {
-    const handleResize = () => {
-      const isMobile = window.innerWidth <= 600;
-      if (!isMobile) {
-        window.location.href = "/";
-      }
-      setIsMobile(isMobile);
-    };
+  // useEffect(() => {
+  //   const handleResize = () => {
+  //     const isMobile = window.innerWidth <= 600;
+  //     if (!isMobile) {
+  //       window.location.href = "/";
+  //     }
+  //     setIsMobile(isMobile);
+  //   };
 
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
+  //   window.addEventListener("resize", handleResize);
+  //   return () => window.removeEventListener("resize", handleResize);
+  // }, []);
 
   return (
     <React.StrictMode>
